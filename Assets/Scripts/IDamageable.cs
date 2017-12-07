8164d5c528633b0889d5338f5b907addcecdbb4d
Chiara <c.wondany@outlook.de>
2017-12-07 21:35:18 +0100

    project setup, basic functions: Moving Player, MapEditor

---
+++ Assets/Scripts/IDamageable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public interface IDamageable {
+
+    // Use this for initialization
+    void TakeHit(float damage, RaycastHit hit);
+
+    void TakeDamage(float damage);
+
+}
